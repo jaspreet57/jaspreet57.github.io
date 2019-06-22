4e465c4dddebf6f9db5539917e1206ce013b4258
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-23 02:14:18 +0530

    setup(code-lint): setups linting and prettier configuration
        * eslint with airbnb, prettier and react config is used.
        * prettier is setup to allow code format plus fix eslint issues
        * husky and lint-staged are used to check for eslint issues before commit.
    
    Changes committed:
    	new file:   .eslintrc
    	new file:   .prettierrc
    	modified:   package.json
    	modified:   src/App.js
    	modified:   src/App.test.js
    	modified:   src/index.js
    	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div> My Portfolio </div>
-  );
+  return <div> My Portfolio </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
