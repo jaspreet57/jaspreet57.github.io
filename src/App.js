d9f0a74c0e963d8c9090d8aee1a025acb0805b7c
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-23 03:36:56 +0530

    setup(styled-component): adds styled-component and required config 	* also removes babel-eslint and eslint as it is already provided by create-react-app
    Changes committed:
    	modified:   package.json
    	modified:   src/App.js
    	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+  color: red;
+  font-size: 24px;
+`;
 
 function App() {
-  return <div> My Portfolio </div>;
+  return (
+    <StyledH1 className="my-header" onClick={() => console.log('clicked')}>
+      <strong>My Portfolio </strong>
+    </StyledH1>
+  );
 }
 
 export default App;
