81175c5f7b6ce92823ab93a9b5634f7941069742
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-24 00:43:20 +0530

    feature(fullpage): adds fullpage feature to scroll through home page content
    Changes committed:
    	modified:   package.json
    	modified:   src/index.js
    	modified:   src/pages/home/index.js
    	modified:   yarn.lock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import App from './App';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Fullpage, Slide } from 'fullpage-react';
 import AppHeader from '../../components/app-header';
 
 
+
+/* Full Page Component is used from https://github.com/cmswalker/fullpage-react#readme */
 function Home() {
-  return (
-    <AppHeader />
-  );
+    const slides = [
+      <Slide style={{background: '#5dfdcb'}}>
+        <AppHeader />
+      </Slide>,
+      <Slide style={{background: '#22333b', color: 'white'}}>
+        Next Section
+      </Slide>,
+    ];
+
+    const fullPageOptions = {
+      // for mouse/wheel events
+      // represents the level of force required to generate a slide change on non-mobile, 10 is default
+      scrollSensitivity: 2,
+    
+      // for touchStart/touchEnd/mobile scrolling
+      // represents the level of force required to generate a slide change on mobile, 10 is default
+      touchSensitivity: 2,
+      scrollSpeed: 500,
+      hideScrollBars: true,
+      enableArrowKeys: true,
+      slides,
+    };
+
+    return <Fullpage {...fullPageOptions} />
 }
 
 export default Home;
