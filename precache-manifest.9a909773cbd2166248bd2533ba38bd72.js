43e40bb8da1ca27bd361eeb8810883ff5d43524e
Jaspreet Singh <erjaspreet57@gmail.com>
2019-08-27 22:27:37 +0530

    Updates

---
+++ precache-manifest.9a909773cbd2166248bd2533ba38bd72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a40138d325c772b24696cc3af31f50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9f8be9e1a29368249e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "86c2c0c5998d4712d8db",
+    "url": "/static/css/main.523118bf.chunk.css"
+  },
+  {
+    "revision": "dc9f8be9e1a29368249e",
+    "url": "/static/js/2.b5fff7d2.chunk.js"
+  },
+  {
+    "revision": "86c2c0c5998d4712d8db",
+    "url": "/static/js/main.48975880.chunk.js"
+  },
+  {
+    "revision": "c1ed24612061233b7cdc",
+    "url": "/static/js/runtime~main.024dd197.js"
+  }
+]);
