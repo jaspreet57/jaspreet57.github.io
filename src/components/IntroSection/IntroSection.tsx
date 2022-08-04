b483bf14d922ba8f74da613bc004edd8dddac65c
Jaspreet Singh <erjaspreet57@gmail.com>
2022-08-04 04:19:17 +0000

    feat(extras): add social icon bar on left side

--- src/components/IntroSection/IntroSection.tsx
+++ src/components/IntroSection/IntroSection.tsx
@@ -19,41 +19,30 @@ const IntroSection: React.FC = () => {
     ContainerBoxStyles = { paddingTop: '100px', display: 'flex' }
     IntroBoxStyles = { width: '50%' }
     ProfileBoxStyles = { width: '50%', zIndex: -2 }
-    profileBoxPositions = { margin: '0 -45px' }
+    profileBoxPositions = { margin: '10px -50px 0 -50px' }
   }
 
   return (
     <Box component="section" sx={ContainerBoxStyles}>
       <Box sx={IntroBoxStyles} component="header">
-        <Typography color="primary" sx={{ fontSize: '28px', fontWeight: boldTextWeight, fontFamily: highlightedFont }}>
+        <Typography color="primary" sx={{ fontSize: '20px', fontWeight: boldTextWeight, fontFamily: highlightedFont }}>
           Hi, my name is
         </Typography>
-        <Typography
-          component="h1"
-          color="secondary"
-          sx={{ fontWeight: boldTextWeight, fontSize: '68px' }}
-        >
+        <Typography component="h1" color="secondary" sx={{ fontWeight: boldTextWeight, fontSize: '65px' }}>
           Jaspreet Singh.
         </Typography>
-        <Typography
-          component="h2"
-          gutterBottom
-          sx={{ fontWeight: boldTextWeight, fontSize: '36px' }}
-        >
-          I'm a Javascript Developer. 
+        <Typography component="h2" gutterBottom sx={{ fontWeight: boldTextWeight, fontSize: '36px' }}>
+          I'm a Javascript Developer.
         </Typography>
-        <Typography
-          component="p"
-          sx={{ fontSize: '20px' }}
-          gutterBottom
-        >
-          I’m a software engineer specializing in building and designing web
-          apps. Currently, I’m focused on developing cloud solutions that helps
-          IOT devices and mobile apps to communicate with each other.
+        <Typography component="p" sx={{ fontSize: '20px' }} gutterBottom>
+          I’m a software engineer specializing in building and designing web apps. Currently, I’m focused on developing
+          cloud solutions that helps IOT devices and mobile apps to communicate with each other.
         </Typography>
-        <Button variant="outlined" color='primary' size="large" sx={{ fontSize: '20px', textTransform: 'none' }}>
-          Checkout My Work
-        </Button>
+        <Box display="flex" justifyContent="center" mt={3}>
+          <Button variant="outlined" color="primary" size="large" sx={{ fontSize: '20px', textTransform: 'none' }}>
+            Checkout My Work
+          </Button>
+        </Box>
       </Box>
 
       <Box sx={ProfileBoxStyles}>
---
+++ src/components/SocialIconsBar/SocialIconsBar.tsx
@@ -0,0 +1,85 @@
+import Box from '@mui/material/Box'
+import useMediaQueryResult from '../../hooks/useMediaQueryResult'
+import { SxProps, Theme, useTheme } from '@mui/material/styles'
+import {
+  FacebookIcon,
+  GithubIcon,
+  InstagramIcon,
+  TwitterIcon,
+  LinkedInIcon,
+  YoutubeIcon,
+} from '../svg-components/feather-icons'
+
+const SocialIconsBar: React.FC = () => {
+  const { isMobileView } = useMediaQueryResult()
+  const theme = useTheme()
+
+  let BoxStyles: SxProps<Theme> = {
+    orientation: 'left',
+    width: '40px',
+    position: 'fixed',
+    left: '40px',
+    right: 'auto',
+    zIndex: 10,
+    top: '50vh',
+    transform: 'translate(-50%, -50%)',
+  }
+
+  let ulStyles: SxProps<Theme> = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '0px',
+    padding: '0px',
+    listStyle: 'none',
+    color: theme.palette.primary.main,
+    '::before, ::after': {
+      content: '""',
+      display: 'block',
+      width: '1px',
+      height: '100vh',
+      margin: '0px auto',
+      backgroundColor: theme.palette.primary.main,
+    },
+    'li': {
+      marginTop: '5px'
+    },
+    'li:first-of-type': {
+      marginTop: '20px',
+    },
+    'li:last-of-type': {
+      marginBottom: '15px',
+    },
+  }
+
+  if (isMobileView) {
+    BoxStyles = { display: 'none' }
+  }
+
+  return (
+    <Box sx={BoxStyles}>
+      <Box component="ul" sx={ulStyles}>
+        <Box component="li">
+          <GithubIcon />
+        </Box>
+        <Box component="li">
+          <LinkedInIcon />
+        </Box>{' '}
+        <Box component="li">
+          <FacebookIcon />
+        </Box>{' '}
+        <Box component="li">
+          <InstagramIcon />
+        </Box>
+        <Box component="li">
+          <TwitterIcon />
+        </Box>
+        <Box component="li">
+          <YoutubeIcon />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default SocialIconsBar
