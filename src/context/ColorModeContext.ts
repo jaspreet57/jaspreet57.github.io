06d96de2f20e19b93df0633fe0b6953283db8d9e
Jaspreet Singh <erjaspreet57@gmail.com>
2023-01-26 19:17:51 +0530

    setup: remove react app

--- src/context/ColorModeContext.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export interface ColorModeContextProps {
-  toggleColorMode: () => void
-}
-
-const ColorModeContext = React.createContext<ColorModeContextProps>({ toggleColorMode: () => {} })
-
-export default ColorModeContext
