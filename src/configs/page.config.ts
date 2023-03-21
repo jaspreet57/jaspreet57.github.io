9043aeaaa3f5dd2bda82155ad169a2d48451d800
Jaspreet Singh <erjaspreet57@gmail.com>
2023-03-21 18:41:22 +0530

    refactor(files): rename config files to .config

--- src/configs/page.config.ts
+++ src/configs/page.config.ts
@@ -1,7 +1,10 @@
-export const SITE = {
-  website: "https://www.jaspreet-singh.com",
+import type { Site } from "src/types";
+
+export const SITE: Site = {
+  website: "https://jaspreets.dev/",
   author: "Jaspreet Singh <erjaspreet57@gmail.com>",
   description:
     "I'm a Javascript Developer. I enjoy creating things that live on the internet.",
   title: "Jaspreet Singh | A Web Developer & Designer",
+  ogImage: "",
 };
